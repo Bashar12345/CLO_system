cadce4bfd4402fc3ff8dce63065421ca0cdf585b
Muhammod Abul Bashar <45920385+Bashar12345@users.noreply.github.com>
2021-09-30 22:28:28 +0600

    init

--- run.py
+++ run.py
@@ -7,10 +7,9 @@ import os
 
 port = int(os.environ.get('PORT', 5000))
 
-
 if __name__ == '__main__':
-   app.run(host='0.0.0.0', port=port, debug=True)
-   # app.run(debug=True, host='0.0.0.0:5000')
+    app.run(host='0.0.0.0', port=port, debug=True)
+    # app.run(debug=True, host='0.0.0.0:5000')
 
 # if __name__ == '__main__':
 #     HOST = environ.get('SERVER_HOST', 'localhost')
