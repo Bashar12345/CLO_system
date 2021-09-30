3170ec472c89e8982d4202c8b16d6cf657040222
Muhammod Abul Bashar <45920385+Bashar12345@users.noreply.github.com>
2021-09-30 17:16:00 +0600

    init

--- run.py
+++ run.py
@@ -3,14 +3,14 @@ from EXAM import create_app
 
 app = create_app()
 
-# if __name__ == '__main__':
-# app.run(debug=True)
-
 if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(environ.get('SERVER_PORT', '5555'))
-    except ValueError:
-        PORT = 5555
-    app.run(HOST, PORT,debug=True)
-    print("server started")
+app.run(debug=True)
+
+# if __name__ == '__main__':
+#     HOST = environ.get('SERVER_HOST', 'localhost')
+#     try:
+#         PORT = int(environ.get('SERVER_PORT', '5555'))
+#     except ValueError:
+#         PORT = 5555
+#     app.run(HOST, PORT,debug=True)
+#     print("server started")
