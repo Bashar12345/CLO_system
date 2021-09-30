9b332e656fa2902b841542b4d1f3eabe1efccafb
Muhammod Abul Bashar <45920385+Bashar12345@users.noreply.github.com>
2021-09-30 22:27:16 +0600

    init

--- run.py
+++ run.py
@@ -3,8 +3,14 @@ from EXAM import create_app
 
 app = create_app()
 
+import os
+
+port = int(os.environ.get('PORT', 5000))
+
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0:5000')
+   app.run(host='0.0.0.0', port=port, debug=True)
+   # app.run(debug=True, host='0.0.0.0:5000')
 
 # if __name__ == '__main__':
 #     HOST = environ.get('SERVER_HOST', 'localhost')
