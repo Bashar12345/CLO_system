02c9495907a1eda34b7db476f3add2e2f62b3564
Muhammad Abul Bashar <45920385+Bashar12345@users.noreply.github.com>
2022-03-21 17:43:30 +0600

    port change

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ app = create_app()
 
 import os
 
-port = int(os.environ.get('PORT', 5000))
+#port = int(os.environ.get('PORT', 5000))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0'debug=True) # port=port, debug=True)
     # app.run(debug=True, host='0.0.0.0:5000')
 
 # if __name__ == '__main__':
