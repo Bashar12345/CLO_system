2f14711fcf2a0e85fe7c7a3c4e0f0d433f16d2b2
Muhammad Abul Bashar <45920385+Bashar12345@users.noreply.github.com>
2022-03-21 17:46:46 +0600

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import os
 #port = int(os.environ.get('PORT', 5000))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0'debug=True) # port=port, debug=True)
+    app.run(host='0.0.0.0',debug=True) # port=port, debug=True)
     # app.run(debug=True, host='0.0.0.0:5000')
 
 # if __name__ == '__main__':
